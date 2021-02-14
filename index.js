f3e45284e02774c5b7161fd2b61a39cbc0f4dadb
Maximilian Pult <pultml@gmail.com>
2021-02-14 18:14:23 +0100

    added rickroll

--- index.js
+++ index.js
@@ -82,4 +82,27 @@ client.on('message', msg => {
         msg.channel.send(embed);
     }
 })
+
+client.on('message', async message => {
+    // Voice only works in guilds, if the message does not come from a guild,
+    // we ignore it
+    if (!message.guild) return;
+    if (message.content.toLowerCase().includes('never')) {
+      // Only try to join the sender's voice channel if they are in one themselves
+        if (message.member.voice.channel) {
+            const connection = await message.member.voice.channel.join();
+            const dispatcher = connection.play('./neverGonnaGiveYouUp.mp3');
+            dispatcher.resume();
+            console.log('rick got rolled')
+
+            dispatcher.on('error', error => {
+                console.log(error)
+            })
+            dispatcher.on('finish', () => {
+                dispatcher.destroy();
+            })
+        }
+    }
+});
+
 client.login(process.env.BOTTOKEN);
